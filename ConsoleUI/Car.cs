1793128781813c1de1bd6d6c50b99b4132b7abb8
Jake Lane <lanejake13@gmail.com>
2020-11-06 15:56:35 -0500

    finished exercise

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Car : Vehicle
+    {
+
+        public bool HasTrunk { get; set; } = true;
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This car is in drive");
+        }
+    }
+}
---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+
+        public bool HasSideCart { get; set; } = false;
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This Motorcycle is in drive!");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is a {GetType().BaseType.Name} is virtually in drive");
+        }
+    }
+}
